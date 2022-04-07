f68a187630048343e4865789a40ac0e970d13cc4
Marija K <kccmrj@outlook.com>
2022-04-07 16:56:08 +0200

    added a css class for each hero image..
    changed .hero into #hero, refactored all code affecting #hero element to resolve the previously described undesirable behaviour of nav bar and logo elements (disappearing and reappearing with hero image on each slide switch with fade effect)

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ fetch("data.json")
   .catch((err) => console.log(`Error: ${err}`));
 
 async function switchData(data) {
-    let image = document.querySelector(".hero");
+    let image = document.querySelector("#hero");
     let title = document.querySelector("h1");
     let description = document.querySelector("article.info > p");
     let index = 0;
     let prev = document.querySelector('.prev');
     let next = document.querySelector('.next');
 
-    image.style = `background-image: url(${data.slides[0].image.desktop})`;
+   // image.style = `background-image: url(${data.slides[0].image.desktop})`;
     title.innerText = `${data.slides[0].title}`;
     description.innerText = `${data.slides[0].description}`;
 
@@ -30,7 +30,7 @@ async function switchData(data) {
          if (index < 0) {
            index = data.slides.length - 1;
          }
-         image.style = `background-image: url(${data.slides[index].image.desktop})`;
+        // image.style = `background-image: url(${data.slides[index].image.desktop})`;
          title.innerText = `${data.slides[index].title}`;
          description.innerText = `${data.slides[index].description}`; 
     })
@@ -40,7 +40,8 @@ async function switchData(data) {
        if (index > data.slides.length - 1) {
          index = 0;
        }
-       image.style = `background-image: url(${data.slides[index].image.desktop})`;
+      // image.style = `background-image: url(${data.slides[index].image.desktop})`;
+       image.setAttribute("class", `img${index}`);
        title.innerText = `${data.slides[index].title}`;
        description.innerText = `${data.slides[index].description}`;
     });
